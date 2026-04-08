25dc49160e2b3d0f01144b22b2d332fd27f2dadb
owusuvictorjnr <owusuvictor80@gmail.com>
2026-04-08 11:37:18 +0000

    refactor: rename ValidationPipe to AppValidationPipe for consistency

--- src/common/pipes/validation/validation.pipe.ts
+++ src/common/pipes/validation/validation.pipe.ts
@@ -1,7 +1,7 @@
 import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
 
 @Injectable()
-export class ValidationPipe implements PipeTransform {
+export class AppValidationPipe implements PipeTransform {
   transform(value: any, metadata: ArgumentMetadata) {
     return value;
   }
