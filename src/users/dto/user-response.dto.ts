327a7347ec72cc363aee3f6f0452709e1e657036
owusuvictorjnr <owusuvictor80@gmail.com>
2026-04-09 09:17:27 +0000

    fix: update UserResponseDto to use camelCase for date fields for consistency

--- src/users/dto/user-response.dto.ts
+++ src/users/dto/user-response.dto.ts
@@ -8,7 +8,7 @@ export class UserResponseDto {
 
   role: UserRole;
   status: UserStatus;
-  created_at: Date;
-  updated_at: Date;
-  deleted_at: Date | null;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt: Date | null;
 }
