2f4fd2e3d783918d8acc3792a218e2589385e5fb
owusuvictorjnr <owusuvictor80@gmail.com>
2026-04-09 14:22:37 +0000

    feat: initialize PrismaService with createPrismaClientOptions

--- src/database/prisma.service.ts
+++ src/database/prisma.service.ts
@@ -1,11 +1,16 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { createPrismaClientOptions } from './prisma-client-options';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor() {
+    super(createPrismaClientOptions());
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
