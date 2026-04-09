f656857e5416a22c8a6af42bb871495d44f3a618
owusuvictorjnr <owusuvictor80@gmail.com>
2026-04-09 08:57:43 +0000

    feat: add FindAllUsersQueryDto for pagination support in user queries

---
+++ src/users/dto/find-all-users-query.dto.ts
@@ -0,0 +1,29 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsInt, IsOptional, Max, Min } from 'class-validator';
+
+export class FindAllUsersQueryDto {
+  @ApiPropertyOptional({
+    description: 'Number of records to skip',
+    default: 0,
+    minimum: 0,
+  })
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(0)
+  skip = 0;
+
+  @ApiPropertyOptional({
+    description: 'Number of records to return',
+    default: 10,
+    minimum: 1,
+    maximum: 100,
+  })
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  @Max(100)
+  take = 10;
+}
