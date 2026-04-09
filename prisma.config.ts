f433ce382cef6d00b055ec908aab1f1432354916
owusuvictorjnr <owusuvictor80@gmail.com>
2026-04-09 14:58:23 +0000

    feat: update datasource URL configuration to include fallback for DATABASE_URL

--- prisma.config.ts
+++ prisma.config.ts
@@ -10,6 +10,8 @@ export default defineConfig({
     seed: "ts-node prisma/seed.ts",
   },
   datasource: {
-    url: process.env["DATABASE_URL"],
+    url:
+      process.env["DATABASE_URL"] ??
+      "postgresql://postgres:postgres@localhost:5432/device_financing?schema=public",
   },
 });
