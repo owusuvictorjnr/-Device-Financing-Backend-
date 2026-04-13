e18a83729a745656acf53d7e46026797164e58c0
owusuvictorjnr <owusuvictor80@gmail.com>
2026-04-13 10:25:22 +0000

    feat: add PrismaService provider to CustomersService tests

--- src/customers/customers.service.spec.ts
+++ src/customers/customers.service.spec.ts
@@ -1,12 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CustomersService } from './customers.service';
+import { PrismaService } from '../database/prisma.service';
 
 describe('CustomersService', () => {
   let service: CustomersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CustomersService],
+      providers: [
+        CustomersService,
+        {
+          provide: PrismaService,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<CustomersService>(CustomersService);
