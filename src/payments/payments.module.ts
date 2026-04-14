5be1c07314e963e6b82c5417cb4948296692c8c8
owusuvictorjnr <owusuvictor80@gmail.com>
2026-04-14 16:01:34 +0000

    feat: add imports for DatabaseModule and LoansModule in PaymentsModule

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from '../database/database.module';
+import { LoansModule } from '../loans/loans.module';
 import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
 
 @Module({
+  imports: [DatabaseModule, LoansModule],
   controllers: [PaymentsController],
   providers: [PaymentsService],
 })
