91afd04a85ba6b4454b40eb432428f4852c2386d
owusuvictorjnr <owusuvictor80@gmail.com>
2026-04-14 16:18:44 +0000

    feat: remove unused module imports from LoansModule

--- src/loans/loans.module.ts
+++ src/loans/loans.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AgentsModule } from '../agents/agents.module';
-import { CustomersModule } from '../customers/customers.module';
 import { DatabaseModule } from '../database/database.module';
-import { DevicesModule } from '../devices/devices.module';
 import { LoansController } from './loans.controller';
 import { LoansService } from './loans.service';
 
 @Module({
-  imports: [DatabaseModule, AgentsModule, CustomersModule, DevicesModule],
+  imports: [DatabaseModule],
   controllers: [LoansController],
   providers: [LoansService],
 })
