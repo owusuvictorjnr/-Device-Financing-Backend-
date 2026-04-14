91764c1cb553881631c6a3cd1a103fe4eee1b91b
owusuvictorjnr <owusuvictor80@gmail.com>
2026-04-14 17:43:23 +0000

    feat: add LoanResponseDto for loan response data structure

---
+++ src/loans/dto/loan-response.dto.ts
@@ -0,0 +1,18 @@
+import { LoanStatus } from '@prisma/client';
+
+export class LoanResponseDto {
+  id!: string;
+  customerId!: string;
+  deviceId!: string;
+  agentId!: string;
+  principalAmount!: number;
+  installmentAmount!: number;
+  durationDays!: number;
+  startDate!: Date;
+  dueDate!: Date;
+  gracePeriodDays!: number;
+  status!: LoanStatus;
+  createdAt!: Date;
+  updatedAt!: Date;
+  deletedAt!: Date | null;
+}
