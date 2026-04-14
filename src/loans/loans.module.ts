5b1b6ec748555982765f02704a5dd8743547fb9f
owusuvictorjnr <owusuvictor80@gmail.com>
2026-04-14 16:29:56 +0000

    feat: remove unused DatabaseModule import from LoansModule

--- src/loans/loans.module.ts
+++ src/loans/loans.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { DatabaseModule } from '../database/database.module';
 import { LoansController } from './loans.controller';
 import { LoansService } from './loans.service';
 
 @Module({
-  imports: [DatabaseModule],
   controllers: [LoansController],
   providers: [LoansService],
 })
