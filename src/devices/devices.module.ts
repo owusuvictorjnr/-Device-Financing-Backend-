2c49fda3af816559e2414ea6efa1be961b12fb07
owusuvictorjnr <owusuvictor80@gmail.com>
2026-04-14 16:00:18 +0000

    refactor: add imports for DatabaseModule and CustomersModule in DevicesModule

--- src/devices/devices.module.ts
+++ src/devices/devices.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from '../database/database.module';
+import { CustomersModule } from '../customers/customers.module';
 import { DevicesController } from './devices.controller';
 import { DevicesService } from './devices.service';
 
 @Module({
+  imports: [DatabaseModule, CustomersModule],
   controllers: [DevicesController],
   providers: [DevicesService],
 })
