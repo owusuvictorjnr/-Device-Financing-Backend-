9ecea98af888f84e209a6bdb24973ddb93dc2ee6
owusuvictorjnr <owusuvictor80@gmail.com>
2026-04-14 17:42:44 +0000

    feat: add DatabaseModule import to LoansModule

--- src/loans/loans.module.ts
+++ src/loans/loans.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from '../database/database.module';
 import { LoansController } from './loans.controller';
 import { LoansService } from './loans.service';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [LoansController],
   providers: [LoansService],
 })
