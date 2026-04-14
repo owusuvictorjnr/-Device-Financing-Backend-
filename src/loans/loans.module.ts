10e864866057fed4dbbff2e6bcc84de2b984ef17
owusuvictorjnr <owusuvictor80@gmail.com>
2026-04-14 16:01:27 +0000

    feat: update LoansModule to include necessary imports for dependencies

--- src/loans/loans.module.ts
+++ src/loans/loans.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { AgentsModule } from '../agents/agents.module';
+import { CustomersModule } from '../customers/customers.module';
+import { DatabaseModule } from '../database/database.module';
+import { DevicesModule } from '../devices/devices.module';
 import { LoansController } from './loans.controller';
 import { LoansService } from './loans.service';
 
 @Module({
+  imports: [DatabaseModule, AgentsModule, CustomersModule, DevicesModule],
   controllers: [LoansController],
   providers: [LoansService],
 })
