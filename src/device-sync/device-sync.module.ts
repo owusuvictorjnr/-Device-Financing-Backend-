a2b823ff12fc413a7d62818a8cdae48c8534fa24
owusuvictorjnr <owusuvictor80@gmail.com>
2026-04-20 08:36:22 +0000

    feat: add DatabaseModule import to DeviceSyncModule

--- src/device-sync/device-sync.module.ts
+++ src/device-sync/device-sync.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from '../database/database.module';
 import { DeviceSyncController } from './device-sync.controller';
 import { DeviceSyncService } from './device-sync.service';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [DeviceSyncController],
   providers: [DeviceSyncService],
 })
