20fe19c96590d76370f40c5438c1748cce296f8f
owusuvictorjnr <owusuvictor80@gmail.com>
2026-04-21 07:35:23 +0000

    feat: add DatabaseModule import to JobsModule

--- src/jobs/jobs.module.ts
+++ src/jobs/jobs.module.ts
@@ -1,15 +1,18 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from '../database/database.module';
 import { JobsService } from './jobs.service';
 import { PaymentEnforcementWorker } from './payment-enforcement.worker/payment-enforcement.worker';
 import { CommandRetryWorker } from './command-retry.worker/command-retry.worker';
 import { ReminderWorker } from './reminder.worker/reminder.worker';
 
 @Module({
+  imports: [DatabaseModule],
   providers: [
     JobsService,
     PaymentEnforcementWorker,
     CommandRetryWorker,
     ReminderWorker,
   ],
+  exports: [JobsService],
 })
 export class JobsModule {}
